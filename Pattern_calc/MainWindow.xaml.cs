c8bafadc0905e09575b66a99df0a657a1bad780c
Настя Быкова <ProtocolApogay@gmail.com>
2023-01-13 18:14:46 +0300

    Добавьте файлы проекта.

---
+++ Pattern_calc/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pattern_calc
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Model.TBone = TBOne;
+            Model.TBtwo = TBTwo;
+            Model.TBsim = TBsim;
+            Model.TBRes = TBRes;
+            CBAr.ItemsSource = Model.dataList;
+        }
+
+        private void BTRes_Click(object sender, RoutedEventArgs e)
+        {
+            if (Model.Res++ == 3)
+            {
+                TBRes.Text = "";
+                MessageBox.Show("Вы не выбрали арифметическую функцию!");
+            }
+            else if (Model.Res++ == 2) 
+            {
+                TBRes.Text = "";
+                MessageBox.Show("Вы не указали второе число!");
+            }
+            else if (Model.Res++ == 1)
+            {
+                TBRes.Text = "";
+                MessageBox.Show("Вы не указали первое число!");
+            }
+            else if (Model.Res++ == 0)
+            {
+                TBRes.Text = "";
+                MessageBox.Show("Ошибка!");
+            }
+        }
+
+        private void CBAr_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Model.Combo = CBAr.SelectedIndex;
+        }
+    }
+}
---
+++ Pattern_calc/Model.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Pattern_calc
+{
+    public static class Model
+    {
+        /// <summary>
+        /// data
+        /// </summary>
+        public static List<string> dataList = new List<string> {"Сложение", "Вычитание", "Умножение", "Деление" };
+        static int a;
+        static int b;
+        static int res;
+
+        public static TextBox TBone;
+        public static TextBox TBtwo;
+        public static TextBlock TBsim;
+        public static TextBlock TBRes;
+
+        public static int Combo 
+        {
+            set 
+            {
+                if (dataList[value] == "Сложение")
+                {
+                    TBsim.Text = "+";
+                }
+                else if (dataList[value] == "Вычитание")
+                {
+                    TBsim.Text = "-";
+                }
+                else if (dataList[value] == "Умножение")
+                {
+                    TBsim.Text = "*";
+                }
+                else if (dataList[value] == "Деление") 
+                {
+                    TBsim.Text = "/";
+                }
+            }
+        }
+
+        public static int Res 
+        {
+            get 
+            {
+                if (TBsim.Text != "") 
+                {
+                    if (TBone.Text != "") 
+                    {
+                        if (TBtwo.Text != "")
+                        {
+                            if (TBsim.Text == "+")
+                            {
+                                TBsim.Text = "+";
+                                a = Convert.ToInt32(TBone.Text);
+                                b = Convert.ToInt32(TBtwo.Text);
+                                res = a + b - 1;
+                                return res;
+                            }
+                            else if (TBsim.Text == "-")
+                            {
+                                TBsim.Text = "-";
+                                a = Convert.ToInt32(TBone.Text);
+                                b = Convert.ToInt32(TBtwo.Text);
+                                res = a - b - 1;
+                                return res;
+                            }
+                            else if (TBsim.Text == "*")
+                            {
+                                a = Convert.ToInt32(TBone.Text);
+                                b = Convert.ToInt32(TBtwo.Text);
+                                res = a * b - 1;
+                                return res;
+                            }
+                            else if (TBsim.Text == "/")
+                            {
+                                a = Convert.ToInt32(TBone.Text);
+                                b = Convert.ToInt32(TBtwo.Text);
+                                res = a / b - 1;
+                                return res;
+                            }
+                            else
+                            {
+                                return 0;
+                            }
+                        }
+                        else 
+                        {
+                            return 2;
+                        }
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+                }
+                else
+                {
+                    return 3;
+                }
+
+            }
+            set 
+            {
+                res = value;
+                TBRes.Text=res.ToString();
+            }
+        }
+    }
+}
